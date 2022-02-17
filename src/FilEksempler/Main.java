be83adc2b8bacf09cf246121179eadb3a729dcc7
susannejung <95688233+susannejung@users.noreply.github.com>
2022-02-17 17:07:40 +0100

    Commit8

--- src/FilEksempler/Main.java
+++ src/FilEksempler/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.print("Commit2");
         System.out.print("Commit3");
         System.out.print("Commit1_1");
+        System.out.print("Commit1_2");
     }
 }
